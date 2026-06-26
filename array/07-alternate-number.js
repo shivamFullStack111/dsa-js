e28dc557d42647ce78fcf3bc961aee736c06375c
shivam <shvam12340987@gmail.com>
2026-06-26 22:50:03 +0530

    print alternate elements of array

---
+++ array/07-alternate-number.js
@@ -0,0 +1,7 @@
+const numbers = [10, 20, 30, 40, 50, 60];
+
+// Step increases by 2 to jump to alternate elements
+for (let i = 0; i < numbers.length; i += 2) {
+    console.log(numbers[i]); 
+}
+// Output: 10, 30, 50
