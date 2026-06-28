7771082d88367bf68bead031a875577f96f23d5b
shivam <shvam12340987@gmail.com>
2026-06-28 21:22:30 +0530

    insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,19 @@
+let arr = [4,5,3,2,1]
+
+let swap = (arr,i, j) => {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+for (let i = 1; i < arr.length; i++) {
+  for (let j = i; j < arr.length; j++) {
+    let current = i
+    while( current>0 && arr[current-1]>arr[current] ){
+        swap(arr,current,current-1)
+        current--
+    }
+  }
+}
+
+console.log(arr)
